02742b618bcddbb2b979338b3e4dd41d7fa0d1b8
Duke_of_Karamel <dukek@localhost.localdomain>
2019-12-08 12:28:13 +0100

    fixes

--- js/WeekScheduleView.js
+++ js/WeekScheduleView.js
@@ -197,7 +197,7 @@ class WeekScheduleView
 
     markReservation($css_class, $row, $collumn,from_minu, to_minu)
     {
-        css_percent = "style=\"margin-left:" + from_minu/60*100 + "%;width:" + to_minu/60*100 + "%;\"";
+        let css_percent = "style=\"margin-left:" + from_minu/60*100 + "%;width:" + to_minu/60*100 + "%;\"";
         this.$container.find(`.week_table .tCont[data-row='${$row}'][data-col='${$collumn}']`).append(`<div ${css_percent}></div>`).addClass($css_class);
     }
 
